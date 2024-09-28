32cab0c1978088e6e7239ced51f2ac444ef8b175
mahmud841 <mahmudsuruj14@gmail.com>
2024-09-29 00:29:41 +0800

    doubly linked list

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,17 @@
+  // if (position == 0)
+    // {
+    //     insert_at_head_function(head, tail, val);
+    // }
+    // else if (position==size(head))
+    // {
+    //     insert_at_tail_function(tail,val);
+    // }
+
+    // else if (position >= size(head))
+    // {
+    //     cout << "Invalid" << endl;
+    // }
+    // else
+    // {
+    //     insert_at_position_functon(head, position, val);
+    // }
